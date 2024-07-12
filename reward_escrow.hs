011d1da34be71990f4b36187341d0ee573916f36
csltx <71733129+fastcd@users.noreply.github.com>
2024-07-12 14:00:00 +0200

    Optimize WASM compilation for Go contracts

--- reward_escrow.hs
+++ reward_escrow.hs
@@ -2,3 +2,4 @@
 // Refactor storage layout for better efficiency
 // Fix security vulnerability in random number generation
 // Update smart contract tests
+// Fix security vulnerability in random number generation
