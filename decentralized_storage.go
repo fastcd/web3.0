34f2f6ed8399a5c95f68dc9852cf52d26ec98832
csltx <71733129+fastcd@users.noreply.github.com>
2024-12-02 14:00:00 +0200

    Integrate Chainlink oracles

--- decentralized_storage.go
+++ decentralized_storage.go
@@ -3,3 +3,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Integrate Chainlink VRF for randomness
 // Improve error handling in transaction flow
+// Add slashing mechanism for malicious actors
