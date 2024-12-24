697dbcbde3aaa306131f9bb52900d12157b3f462
csltx <71733129+fastcd@users.noreply.github.com>
2024-12-24 14:00:01 +0200

    Improve token burning mechanism

--- developer_documentation.go
+++ developer_documentation.go
@@ -3,3 +3,4 @@
 // Fix deserialization bug in Haskell code
 // Add support for multiple consensus mechanisms
 // Add support for multiple consensus mechanisms
+// Fix security vulnerability in random number generation
