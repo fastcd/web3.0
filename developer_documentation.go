df84d3131da36a290f202bfa95586388951ad45c
csltx <71733129+fastcd@users.noreply.github.com>
2024-12-25 14:00:00 +0200

    Improve gas estimation for transactions

--- developer_documentation.go
+++ developer_documentation.go
@@ -4,3 +4,4 @@
 // Add support for multiple consensus mechanisms
 // Add support for multiple consensus mechanisms
 // Fix security vulnerability in random number generation
+// Fix memory leak in Rust smart contract
